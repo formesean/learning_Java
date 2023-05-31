d3aae81a5e5b75f32dcf4d1f54435ce801c86670
Sean Aguilar <seanaguilar698@gmail.com>
2023-05-31 13:58:18 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Enter your last name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Country: ");
+        String country = scanner.nextLine();
+
+        System.out.println("\nFull Name: " + firstName + " " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println(country);
+    }
+}
