581066c9972fdf94b45ae9d3d66bb750374def3f
Sean Aguilar <seanaguilar698@gmail.com>
2023-06-02 16:08:55 +0800

    constructors

---
+++ src/OOP/Formula_One/Drivers.java
@@ -0,0 +1,15 @@
+package OOP.Formula_One;
+
+public class Drivers {
+    String name;
+    String team;
+    int age;
+    String nationality;
+
+    Drivers(String name, String team, int age, String nationality) {
+        this.name = name;
+        this.team = team;
+        this.age = age;
+        this.nationality = nationality;
+    }
+}
--- src/OOP/Formula_One/Main.java
+++ src/OOP/Formula_One/Main.java
@@ -7,11 +7,40 @@ public class Main {
         Teams myTeam = new Teams();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Choose a number between 1 to 10: ");
-        int num = scanner.nextInt();
+        System.out.print("Enter number of drivers: ");
+        int numDriver = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.println("\nYour Formula Team is:");
-        System.out.printf("\tTeam: \t\t\t%s\n\tChassis: \t\t%s\n\tPower Unit: \t%s", myTeam.team[num - 1], myTeam.chassis[num - 1], myTeam.powerUnit[num - 1]);
+        Drivers[] driversArray = new Drivers[numDriver];
+
+        for (int i = 0; i < numDriver; i++) {
+            System.out.printf("\nDriver %d name: ", i + 1);
+            String name = scanner.nextLine();
+            System.out.printf("Driver %d team: ", i + 1);
+            String team = scanner.nextLine();
+            System.out.printf("Driver %d age: ", i + 1);
+            int age = scanner.nextInt();
+            scanner.nextLine();
+            System.out.printf("Driver %d nationality: ", i + 1);
+            String nationality = scanner.nextLine();
+
+            driversArray[i] = new Drivers(name, team, age, nationality);
+        }
+
+        System.out.println();
+        for (int j = 0; j < numDriver; j++) {
+            System.out.println("Driver " + (j + 1) + ": " +
+                    driversArray[j].name + ", " +
+                    driversArray[j].team + ", " +
+                    driversArray[j].age + ", " +
+                    driversArray[j].nationality);
+        }
+
+//        System.out.print("Choose a number between 1 to 10: ");
+//        int num = scanner.nextInt();
+
+//        System.out.println("\nYour Formula Team is:");
+//        System.out.printf("\tTeam: \t\t\t%s\n\tChassis: \t\t%s\n\tPower Unit: \t%s", myTeam.team[num - 1], myTeam.chassis[num - 1], myTeam.powerUnit[num - 1]);
 //        myTeam.drsON();
 //        myTeam.drsOFF();
     }
